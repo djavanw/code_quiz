fa8317e7020fd48b76927a89e89433e023f43618
Javan <71285290+djavanw@users.noreply.github.com>
2021-02-28 23:24:09 -0700

    Major revisions. Fixed the questions.  Setup question transition time after selection. Setup game timer. Added a question counter.  Fixed the loop for the questions.

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,176 @@
-var startEl = document.getElementById('start');
-var answerEl =document.getElementsByClassName('answers');
-//var questionEl = document.getElementById('question');
-//var answerOne = document.querySelector('.answer1');
-var elScore;
-var questArray = ['Here is question number one(1)?', 'Here is question number two(2)?', 'Here is question number three(3)?','Here is question number four(4)?', 'Here is question number five(5)?' ];
-var answ1Array = ['a. quest1', 'a. quest2','a. quest3','a. quest4','a. quest5'];
-var answ2Array = ['b. quest1', 'b. quest2','b. quest3','b. quest4','b. quest5'];
-var answ3Array = ['c. quest1', 'c. quest2','c. quest3','c. quest4','c. quest5'];
-var answ4Array = ['d. quest1', 'd. quest2','d. quest3','d. quest4','d. quest5'];
-var correctArray = ['c1','a2','b3','d3','a5'];
+var startEl = document.querySelector('#start');
+var highScoreEl = document.querySelector('#hiScore');
+var scoreEl = document.querySelector('#score');
+var timerEl = document.querySelector('#timer');
+var questionEl = document.querySelector('#question');
+var answersEl = document.querySelector('.answers');
 
-var elTimer;
-var elHighScore;
+var answerOne = document.querySelector('#answ1');
+var answerTwo = document.querySelector('#answ2');
+var answerThree = document.querySelector('#answ3');
+var answerFour = document.querySelector('#answ4');
+var correctEl = document.querySelector('#correct');
 
-function generateQuestion() {
-    for(let i = 0; i <questArray.length; i++) {
-    document.getElementById('question').textContent = questArray[i];
-    document.getElementById('answ1').textContent = answ1Array[i];
-    document.getElementById('answ2').textContent = answ2Array[i];
-    document.getElementById('answ3').textContent = answ3Array[i];
-    document.getElementById('answ4').textContent = answ4Array[i];
-    
-    answerEl.addEventListener('click');
-    if('answer' === correctArray[i]) {
-        console.log('The answer is correct');
-    } else {
-        console.log('The answer is wrong');
-        }
+let playTime = 75;
+
+var questAnsw = [
+    {
+        0: 'Here is question number one(1)?',
+        1: 'Answer A to question1',
+        2: 'Answer B to question1',
+        3: 'Answer C to question1',
+        4: 'Answer D to question1',
+        5: 'c'
+    },
+
+    {
+        0: 'Here is question number two(2)?',
+        1: 'Answer A to question2',
+        2: 'Answer B to question2',
+        3: 'Answer C to question2',
+        4: 'Answer D to question2',
+        5: 'd'
+    },
+
+    {
+        0: 'Here is question number three(3)?',
+        1: 'Answer A to question3',
+        2: 'Answer B to question3',
+        3: 'Answer C to question3',
+        4: 'Answer D to question3',
+        5: 'a'
+    },
+
+    {
+        0: 'Here is question number four(4)?',
+        1: 'Answer A to question4',
+        2: 'Answer B to question4',
+        3: 'Answer C to question4',
+        4: 'Answer D to question4',
+        5: 'b'
+    },
+
+    {
+        0: 'Here is question number five(5)?',
+        1: 'Answer A to question5',
+        2: 'Answer B to question5',
+        3: 'Answer C to question5',
+        4: 'Answer D to question5',
+        5: 'c'
     }
+];
 
+let j = 0;
+let i;
+let currentQuestion = j;
+//This is the transition time between questions after selection
+//while(j < questAnsw.length)
+//for(j = 0; j < questAnsw.length; i++)
+
+
+function genQuestion() {
+   console.log('CurrentQuestion: ' + (currentQuestion +1))
+        
+        //questionEl.textContent = questAnsw[j][i];
+        //answerOne.innerHTML = questAnsw[j][i];
+    
+    for(i = 0; i < questAnsw.length; i++)
+    {
+        if(i === 0) {
+            questionEl.innerHTML = questAnsw[j][i];
+            console.log(questAnsw[j][i])
+               
+        } else if(i === 1) {
+            answerOne.innerHTML = questAnsw[j][i];
+            console.log(questAnsw[j][i])
+
+        } else if(i === 2) {
+            answerTwo.innerHTML = questAnsw[j][i];
+            console.log(questAnsw[j][i])
+
+        } else if(i === 3) {
+            answerThree.innerHTML = questAnsw[j][i];
+            console.log(questAnsw[j][i])
+
+        } else if(i === 4) {
+            answerFour.innerHTML = questAnsw[j][i];
+            console.log(questAnsw[j][i])
+
+        } else {console.log('iteration finished')}
+       
+    answerOne.addEventListener('click', transitionEl); 
+    answerTwo.addEventListener('click', transitionEl);
+    answerThree.addEventListener('click', transitionEl);
+    answerFour.addEventListener('click', transitionEl);
+
+    console.log('Loop finished ' + i);
+    
+    }   
+       
+}
+
+function additionEl(){
+    console.log('You are in a new function');
+    // console.log(event.type);
+    // console.log(event.target);
+    j = j + 1;
+    currentQuestion++;
+    
+    //transitionEl();
+    
+
+    //if(event.target === questAnsw[j][3])
+    console.log('You are finished');
+    genQuestion();
 };
 
-generateQuestion();
 
 
-//startEl.addEventListener('click', postQuest);
+function transitionEl() {
+    let transitionTime = 3;
+    console.log('Made it to the Question transition timer')
+    var timerInterval = setInterval(function(){
+        transitionTime--;
+
+        questionEl.textContent = ' ';
+        answerOne.innerHTML = ' ';
+        answerTwo.innerHTML = ' ';
+        answerThree.innerHTML = ' ';
+        answerFour.innerHTML = ' ';
+
+        if(transitionTime === 0) {
+            clearInterval(timerInterval);
+            
+            questionEl.textContent = questAnsw[j][i];
+            answerOne.innerHTML = questAnsw[j][i];
+            answerTwo.innerHTML = questAnsw[j][i];
+            answerThree.innerHTML = questAnsw[j][i];
+            answerFour.innerHTML = questAnsw[j][i];
+
+            additionEl();
+        }
+    }, 1000);
+}
+
+function timerGame(event) {
+    
+    console.log('This timer is going');
+    console.log(event);
+    var timerInterval = setInterval(function(){
+        playTime--;
+        timerEl.innerHTML = playTime;
+        if(playTime === 0) {
+            clearInterval(timerInterval);
+            timerEl.innerHTML = 'Time is Up'
+         }
+    }, 1000);
+}
+
+
+
+startEl.addEventListener('click', genQuestion);  //This starts the application  
+startEl.addEventListener('click', timerGame);
+
+
+
+    
